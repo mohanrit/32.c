989be8df4e102654818d4c32abb5b75f3e057389
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-02-28 16:57:23 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    int v,i;
+    scanf("%d",&v);
+    for(i=0;i<a[i];i++)
+    {
+        if(a[10]>v)
+        {
+        printf(" yes");
+        }
+    else
+    {
+    printf("no");
+    }
+        
+    }
+}
